5258adbd3abec12981e4d9b4c1b1eb4c3529b6ad
Yuji Sasaki <jyu0414@gmail.com>
2020-03-08 10:56:50 +0900

    Initial Commit

---
+++ sumiTests/sumiTests.swift
@@ -0,0 +1,34 @@
+//
+//  sumiTests.swift
+//  sumiTests
+//
+//  Created by Yuji Sasaki on 2020/03/08.
+//  Copyright © 2020 Yuji Sasaki. All rights reserved.
+//
+
+import XCTest
+@testable import sumi
+
+class sumiTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
